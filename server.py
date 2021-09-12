78d71c06ef1f4a45b0898e67c5204367f613a9ce
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-09-13 02:58:13 +0430

    Create a TCP/IP socket

--- server.py
+++ server.py
@@ -5,7 +5,13 @@ import threading
 
 PORT = 5050
 # IP address of your computer
-#SERVER = "192.168.1.104"
+# SERVER = "192.168.1.104"
 SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
-print(SERVER)
+# create an INET, STREAMing socket (TCP/IP)
+# AF_INET: Address Family of Internet Protocol v4
+# SOCK_STREAM:  TCP Socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to address
+server.bind(ADDR)
