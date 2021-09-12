3b187862a3ab06561a4df33f666ff5c52aa8f6e2
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-09-13 02:43:53 +0430

    Specify server IP address

--- server.py
+++ server.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+import socket
+import threading
+
+PORT = 5050
+# IP address of your computer
+#SERVER = "192.168.1.104"
+SERVER = socket.gethostbyname(socket.gethostname())
+
+print(SERVER)
