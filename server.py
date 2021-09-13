a9a39fe1b39a6b714a11bcfb642d07d4bfb0ebc8
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-09-13 13:52:04 +0430

    Write the server socket program

--- server.py
+++ server.py
@@ -3,11 +3,17 @@
 import socket
 import threading
 
+# The port that we want to bind to our server
 PORT = 5050
 # IP address of your computer
-# SERVER = "192.168.1.104"
+# SERVER = "127.0.0.1"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+
+# Specification of my own custom protocol
+HEADER_SIZE = 64
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
 
 # create an INET, STREAMing socket (TCP/IP)
 # AF_INET: Address Family of Internet Protocol v4
@@ -15,3 +21,45 @@ ADDR = (SERVER, PORT)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to address
 server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    isConnected = True
+    print(f"[NEW CONNECTION] {addr} connected.")
+    # Keep receiving messages until the DISCONNECT_MSG arrives
+    while isConnected:
+        # First receive the size of incoming message
+        msgLength = conn.recv(HEADER_SIZE).decode(FORMAT)
+        msgLength = int(msgLength)
+        # No receive the main message according to the size received before
+        msg = conn.recv(msgLength).decode(FORMAT)
+
+        if msg == DISCONNECT_MSG:
+            isConnected = False
+
+        print(f"[{addr}] {msg}")
+
+    # Close the client's socket connection
+    conn.close()
+
+
+def start():
+    # Enable the server to accept connections
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+
+    while True:
+        # Wait for a new connection to the server
+        # conn: Connection to the client used for responding
+        # addr: Address of the client
+        conn, addr = server.accept()
+        # Create a separate thread for handling the new client connection
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        # Number of clients equals number of threads created for clients minus the main thread
+        clientsCount = threading.active_count() - 1
+        print(f"[ACTIVE CLIENT CONNECTIONS] {clientsCount}")
+
+
+print("[STARTING] server is starting ...")
+start()
